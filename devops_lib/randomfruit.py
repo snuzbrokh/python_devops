eb8f0d6af721b448821bbd3951cceb9b6d88d31f
Sam N <snuzbrokh@gmail.com>
2022-09-19 14:59:33 -0700

    fixed space

--- devops_lib/randomfruit.py
+++ devops_lib/randomfruit.py
@@ -9,8 +9,7 @@ def fruit():
 
 def meal(beverage):
     my_fruit = fruit()
-    if my_fruit == 'kiwi':
+    if my_fruit == "kiwi":
         complete_meal = f"Your meal is a {my_fruit} and {beverage}"
         return complete_meal
     return f"Have some steak and {beverage}"
-    
--- test_devopslib.py
+++ test_devopslib.py
@@ -4,6 +4,8 @@ from devops_lib.randomfruit import fruit, meal
 def test_fruit():
     fruit_choice = fruit()
     assert fruit_choice in ["apple", "banana", "kiwi"]
+
+
 def test_meal():
     results = meal("MILK")
     assert "MILK" in results
